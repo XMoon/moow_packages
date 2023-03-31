ad517fa25c7199856c242fe4c4d00f7dcc8f6e83
XMoon <xmoonox@gmail.com>
2023-03-31 18:28:54 +0800

    [script] add update_subtree.sh

---
+++ update_subtree.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git subtree pull --prefix=phantun https://github.com/XMoon/phantun-openwrt master
+git subtree pull --prefix=smartdns https://github.com/pymumu/openwrt-smartdns/ master
+git subtree pull --prefix=luci-app-smartdns https://github.com/pymumu/luci-app-smartdns master
+git subtree pull --prefix=luci-theme-argon https://github.com/jerrykuku/luci-theme-argon master
