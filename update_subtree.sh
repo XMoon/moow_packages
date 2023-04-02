70abead4ae39522822e6c996272e2ed54400542f
XMoon <xmoonox@gmail.com>
2023-04-02 23:48:38 +0800

    [script] add ddns-go

--- update_subtree.sh
+++ update_subtree.sh
@@ -4,3 +4,4 @@ git subtree pull --prefix=phantun https://github.com/XMoon/phantun-openwrt maste
 git subtree pull --prefix=smartdns https://github.com/pymumu/openwrt-smartdns/ master
 git subtree pull --prefix=luci-app-smartdns https://github.com/pymumu/luci-app-smartdns master
 git subtree pull --prefix=luci-theme-argon https://github.com/jerrykuku/luci-theme-argon master
+git subtree pull --prefix=ddns-go-packages https://github.com/sirpdboy/luci-app-ddns-go.git main
